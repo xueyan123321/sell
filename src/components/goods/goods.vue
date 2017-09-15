2bf00d0e2d20f0100231f2ee00294f4bea6cd2d3
luoke <1152991535@qq.com>
2017-09-15 16:55:34 +0800

    have add something

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -3,15 +3,42 @@
     <div class="menu-wrapper">
       <ul>
         <li v-for="item in goods" class="menu-item">
-
+          <icon :iconType="item.type" :text="item.name"></icon>
         </li>
       </ul>
     </div>
-    <div class="foods-wrapper"></div>
+    <div class="foods-wrapper">
+      <ul>
+        <li v-for="item in goods" class="food-list">
+          <h1 class="title">{{item.name}}</h1>
+          <ul>
+            <li v-for="food in item.foods" class="food-item border-1px">
+              <div class="icon">
+                <img  :src="food.icon">
+              </div>
+              <div class="content">
+                <h2 class="name">{{food.name}}</h2>
+                <p class="desc">{{food.description}}</p>
+                <div class="extra">
+                  <span class="count">月售{{food.sellCount}}份</span>
+                  <span>好评率{{food.rating}}%</span>
+                </div>
+                <div class="price">
+                  <span class="now">￥{{food.price}}</span>
+                  <span class="old" v-show="food.oldPrice">￥
+                    {{food.oldPrice}}</span>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import icon from '../icon/icon';
   const ERR_OK = 0;
   export default {
     props: {
@@ -26,12 +53,14 @@
       };
     },
     created() {
-      this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
       this.axios.get('/api/goods').then((response) => {
         if (response.data.errno === ERR_OK) {
           this.goods = response.data.data;
         }
       });
+    },
+    components: {
+      icon
     }
   };
 </script>
@@ -58,41 +87,73 @@
         padding: 0 12px;
         line-height:14px;
       }
-      .icon{
-        display:inline-block;
-        width: 12px;
-        height: 12px;
-        vertical-align: top;
-        margin-right: 2px;
-        background-size: 16px 16px;
-        background-repeat: no-repeat;
-        &.decrease{
-          @include bg-image('decrease_3')
-        }
-        &.discount{
-          @include bg-image('discount_3')
-        }
-        &.guarantee{
-          @include bg-image('guarantee_3')
-        }
-        &.invoice{
-          @include bg-image('invoice_3')
-        }
-        &.special{
-          @include bg-image('special_3')
-        }
-      }
-      .text{
-        display: table-cell;
-        width:56px;
-        vertical-align: middle;
-        @include border-1px(rgba(7,17,27,0.1));
-        font-size: 12px;
-      }
     }
     .foods-wrapper
     {
       flex: 1;
+      .title{
+        padding-left: 14px;
+        height: 26px;
+        line-height: 26px;
+        border-left: 2px solid #d9dde1;
+        font-size:12px;
+        color:rgb(147,153,159);
+        background: #f3f5f7;
+      }
+      .food-item{
+        display:flex;
+        margin: 18px;
+        padding-bottom: 18px;
+        @include border-1px(rgba(7,17,27,0.1));
+        &:last-child{
+          @include border-none();
+          margin-bottom: 0;
+          .icon{
+            flex: 0 0 57px;
+            margin-right: 10px;
+          }
+        }
+        .content{
+          flex: 1;
+          .name{
+            margin: 2px 0 8px 0;
+            height: 14px;
+            line-height: 14px;
+            font-size: 14px;
+            color:rgb(7,17,27)
+          }
+          .desc,.extra{
+            margin-bottom: 8px;
+            line-height: 10px;
+            font-size:10px;
+            color:rgb(147,153,159)
+          }
+          .desc{
+            margin-bottom: 8px;
+
+          }
+          .extra{
+            line-height: 10px;
+            &.count{
+              margin-right: 12px;
+            }
+          }
+          .price{
+            font-weight: 700;
+            line-height: 24px;
+            .now{
+              margin-right: 8px;
+              font-size:14px;
+              color:rgb(240,20,20)
+            }
+            .old{
+              text-decoration: line-through;
+              font-size:10px;
+              color:rgb(147,153,159)
+            }
+          }
+        }
+      }
     }
   }
 
--- src/components/icon/icon.vue
+++ src/components/icon/icon.vue
@@ -1,19 +1,30 @@
 <template>
     <span class="text border-1px">
-      <span class="icon" :class="classMap[item.type]"></span>{{item.name}}
+      <span v-show="iconType>0" class="icon" :class="classMap[iconType]"></span>{{text}}
     </span>
 </template>
 
 <script type="text/ecmascript-6">
-
+  export default {
+    props: {
+      iconType: Number,
+      text: String
+    },
+    data() {
+      return {
+        classMap: ['decrease', 'discount', 'special', 'invoice', 'guarantee']
+      };
+    }
+  };
 </script>
 
 <style lang="scss"
-       rel="stylesheet/scss">
+       rel="stylesheet/scss" scoped>
+  @import "../../common/sass/mixin.scss";
   .icon{
     display:inline-block;
-    width: 12px;
-    height: 12px;
+    width: 18px;
+    height: 18px;
     vertical-align: top;
     margin-right: 2px;
     background-size: 16px 16px;
@@ -33,5 +44,12 @@
     &.special{
       @include bg-image('special_3')
     }
+  }
+  .text{
+    display: table-cell;
+    width:56px;
+    vertical-align: middle;
+    @include border-1px(rgba(7,17,27,0.1));
+    font-size: 12px;
   }
 </style>
